1edee1b6b2dfd577504723a5ff77a901b0b88bff
jason goldfarb <jasongoldfarb@jasons-MacBook-Pro.local>
2015-08-05 16:30:47 -0600

    Checkpoint 32 CRUD completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,7 @@
 Rails.application.routes.draw do
-  get 'posts/index'
+  resources :posts
 
-  get 'posts/show'
-
-  get 'posts/new'
-
-  get 'posts/edit'
-
-  get 'welcome/index'
-
-  get 'welcome/about' 
-
-  get 'welcome/contact'
+  get 'about' => 'welcome#about'
 
  root to: 'welcome#index'
 end

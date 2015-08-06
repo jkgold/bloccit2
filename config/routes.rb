0108a38db6d699c302c265f83dd015cb7c656563
jason goldfarb <jasongoldfarb@jasons-MacBook-Pro.local>
2015-08-06 08:19:23 -0600

    checkpoint 33 CRUD compled

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  resources :posts
 
-  get 'about' => 'welcome#about'
-
-=======
  resources :posts
- 
+
  get 'about' => 'welcome#about'
- 
->>>>>>> master
+
  root to: 'welcome#index'
 end
